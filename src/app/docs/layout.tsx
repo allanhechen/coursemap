a81ca1ea0d2791740d3720119ff9ebbd4e060323
Allan He-Chen <allanhechen@gmail.com>
2024-12-16 20:30:17 -0500

    Add additional support for documentation

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -2,10 +2,22 @@ import source from "@/app/lib/source";
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import type { ReactNode } from "react";
 import { baseOptions } from "@/app/layout.config";
+import WideLogo from "@/app/components/header/WideLogo";
+import Link from "next/link";
 
 export default function Layout({ children }: { children: ReactNode }) {
     return (
-        <DocsLayout tree={source.pageTree} {...baseOptions}>
+        <DocsLayout
+            tree={source.pageTree}
+            {...baseOptions}
+            sidebar={{
+                banner: (
+                    <Link href="/docs">
+                        <WideLogo />
+                    </Link>
+                ),
+            }}
+        >
             {children}
         </DocsLayout>
     );
