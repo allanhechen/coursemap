63ed48513f398746a4a8ca75d855311a4d3a6363
Allan He-Chen <allanhechen@gmail.com>
2024-12-24 20:20:52 -0600

    Style the DeleteArea component a bit nicer

--- src/components/DeleteArea.tsx
+++ src/components/DeleteArea.tsx
@@ -1,16 +1,16 @@
-import { Paper } from "@mantine/core";
+import { Paper, Text } from "@mantine/core";
 
 export default function DeleteArea() {
     return (
         <Paper
-            className="nodrag"
+            className="nodrag flex justify-center pt-5"
             shadow="sm"
             style={{
                 width: "80vw",
                 height: "20vh",
             }}
         >
-            DeleteArea
+            <Text size="xl">Drop Course Here to Delete</Text>
         </Paper>
     );
 }
