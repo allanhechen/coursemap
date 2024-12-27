f3465443c59f072a5d0d3ff6c128923237223ad0
Allan He-Chen <allanhechen@gmail.com>
2024-12-26 22:53:39 -0600

    Add horizontal scrolling for semester and card

--- src/components/semester/Semester.tsx
+++ src/components/semester/Semester.tsx
@@ -10,31 +10,51 @@ export default function Semester({ data }: SemesterWrapper) {
     const { getNode } = useReactFlow();
     const node_id = useNodeId() as string; // we are sure this is a string
     const [lastY, setLastY] = useState(0);
-    const { scrollHandler } = useScrollHandler();
+    const [lastX, setLastX] = useState(0);
+    const { verticalScrollHandler, horizontalScrollHandler } =
+        useScrollHandler();
 
     const onWheel = useCallback(
         (event: React.WheelEvent) => {
-            const self = getNode(node_id) as Node; // we know for sure this node exists
-            const deltaY = event.deltaY;
-            scrollHandler(self, deltaY);
+            const { deltaX, deltaY } = event;
+            // Only scroll horizontal or vertical
+            if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                horizontalScrollHandler(deltaX);
+            } else {
+                const self = getNode(node_id) as Node; // we know for sure this node exists
+                verticalScrollHandler(self, deltaY);
+            }
         },
-        [scrollHandler, getNode, node_id]
+        [verticalScrollHandler, horizontalScrollHandler, getNode, node_id]
     );
 
-    // usecallback depends on lastY, only call it if lastY changes
     const onTouchMove = useCallback(
         (event: React.TouchEvent) => {
-            const self = getNode(node_id) as Node; // we know for sure this node exists
-            const currentY = event.changedTouches[0].pageY;
-            const deltaY = currentY - lastY;
-            scrollHandler(self, deltaY);
-            setLastY(currentY);
+            const { pageX, pageY } = event.changedTouches[0];
+            if (Math.abs(pageX) > Math.abs(pageY)) {
+                const deltaX = pageX - lastX;
+                horizontalScrollHandler(deltaX);
+                setLastX(pageX);
+            } else {
+                const self = getNode(node_id) as Node; // we know for sure this node exists
+                const deltaY = pageY - lastY;
+                verticalScrollHandler(self, deltaY);
+                setLastY(pageY);
+            }
         },
-        [lastY, scrollHandler, getNode, node_id]
+        [
+            lastY,
+            lastX,
+            verticalScrollHandler,
+            horizontalScrollHandler,
+            getNode,
+            node_id,
+        ]
     );
 
     const onTouchStart = useCallback((event: React.TouchEvent) => {
         setLastY(event.changedTouches[0].pageY);
+        setLastX(event.changedTouches[0].pageX);
     }, []);
 
     return (
