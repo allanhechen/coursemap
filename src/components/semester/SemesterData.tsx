747981d133648acfcab30b104446ccaa5aa47158
Allan He-Chen <allanhechen@gmail.com>
2025-01-02 15:50:03 -0500

    Add documentation for Semester and related Components

--- src/components/semester/SemesterData.tsx
+++ src/components/semester/SemesterData.tsx
@@ -2,12 +2,5 @@ import SemesterForm from "@/components/semester/SemesterForm";
 import { SemesterInformation } from "@/types/semester";
 
 export default function SemesterData(props: SemesterInformation) {
-    return (
-        <SemesterForm
-            semesterId={props.semesterId}
-            semesterName={props.semesterName}
-            semesterYear={props.semesterYear}
-            semesterTerm={props.semesterTerm}
-        />
-    );
+    return <SemesterForm {...props} />;
 }
