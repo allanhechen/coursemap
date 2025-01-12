2ebe7c9277eaf700d6907d575864f2fd40636b1a
Allan He-Chen <allanhechen@gmail.com>
2025-01-11 20:57:22 -0500

    Add required types

--- src/types/courseCard.ts
+++ src/types/courseCard.ts
@@ -1,5 +1,6 @@
 import { XYPosition } from "@xyflow/react";
 import { ChipVariant } from "@/types/chipVariant";
+import { SemesterInformation } from "@/types/semester";
 
 export interface CardWrapper {
     type?: string;
@@ -7,6 +8,34 @@ export interface CardWrapper {
     data: CourseInformation;
     isConnectable?: boolean;
     position?: XYPosition;
+}
+
+export type CourseToSemesterIdDict = {
+    [courseCode: string]: number | undefined;
+};
+
+export type CourseCodeToSemester = [
+    CourseToSemesterIdDict,
+    React.Dispatch<React.SetStateAction<CourseToSemesterIdDict>>
+];
+
+export interface DropdownCardWrapper {
+    type?: string;
+    id?: string;
+    data: CourseDropdownInformation;
+    isConnectable?: boolean;
+    position?: XYPosition;
+}
+
+export interface CourseDropdownInformation {
+    courseInformation: CourseInformation;
+    semesters: {
+        [semesterId: number]: SemesterInformation;
+    };
+    selectSemester: (
+        courseCode: string,
+        semesterId: number | undefined
+    ) => void;
 }
 
 export interface CourseInformation {
