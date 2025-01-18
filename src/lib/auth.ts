af0366171f7519fdc195abb3e117ad5801d61380
Allan He-Chen <allanhechen@gmail.com>
2025-01-18 15:19:08 -0500

    Extend user session with institutionId and also programName

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,22 @@
 import "@/lib/db";
 import ws from "ws";
 import { Pool, neonConfig } from "@neondatabase/serverless";
-
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { GitHubProfile } from "next-auth/providers/github";
 import PostgresAdapter from "@auth/pg-adapter";
+
+import { getGitHubUserProgram, getUserProgram } from "@/actions/user";
+
+declare module "next-auth" {
+    interface Session {
+        user: {
+            userId?: number;
+            institutionId?: number;
+            programName?: string;
+        } & DefaultSession["user"];
+    }
+}
 
 export const { handlers, auth, signIn, signOut } = NextAuth(() => {
     neonConfig.webSocketConstructor = ws;
@@ -12,5 +24,25 @@ export const { handlers, auth, signIn, signOut } = NextAuth(() => {
     return {
         adapter: PostgresAdapter(pool),
         providers: [GitHub],
+        async profile(profile: GitHubProfile) {
+            const userInformation = await getGitHubUserProgram(profile.id);
+            return {
+                ...profile,
+                ...userInformation,
+            };
+        },
+        callbacks: {
+            async session({ session }) {
+                const userId = parseInt(session.user.id);
+                session.user.userId = userId;
+                const { institutionId, programName } = await getUserProgram(
+                    userId
+                );
+                session.user.institutionId = institutionId;
+                session.user.programName = programName;
+
+                return session;
+            },
+        },
     };
 });
