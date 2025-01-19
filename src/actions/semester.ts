1b6acf88ff07179f0405bde9181724aa8fb5c328
Allan He-Chen <allanhechen@gmail.com>
2025-01-19 10:47:12 -0500

    Create semester server actions for the database

--- src/actions/semester.ts
+++ src/actions/semester.ts
@@ -4,6 +4,39 @@ import { sql } from "@/lib/db";
 
 import { SemesterInformation, SemesterTerm } from "@/types/semester";
 
+export async function getSemesters(
+    userId: number,
+    institutionId: number,
+    programName: string
+): Promise<SemesterInformation[]> {
+    const queryResult = await sql.query<{
+        semesterid: number;
+        semestername: string;
+        semesteryear: number;
+        semesterterm: SemesterTerm;
+    }>(
+        `
+        SELECT semesterid, semestername, semesteryear, semesterterm
+        FROM semester
+        WHERE userId = $1 AND institutionid = $2 AND programname = $3;
+        `,
+        [userId, institutionId, programName]
+    );
+
+    const result: SemesterInformation[] = [];
+    queryResult.rows.forEach(
+        ({ semesterid, semestername, semesterterm, semesteryear }) => {
+            result.push({
+                semesterId: semesterid,
+                semesterName: semestername,
+                semesterTerm: semesterterm,
+                semesterYear: new Date(semesteryear, 0, 1),
+            });
+        }
+    );
+    return result;
+}
+
 export async function createSemester(
     userId: number,
     institutionId: number,
@@ -16,18 +49,18 @@ export async function createSemester(
         `
         INSERT INTO semester(
             userid,
-            programname,
             institutionid,
+            programname,
             semestername,
             semesteryear,
             semesterterm
         ) VALUES (
-            ?,
-            ?,
-            ?,
-            ?,
-            ?,
-            ?
+            $1,
+            $2,
+            $3,
+            $4,
+            $5,
+            $6
         ) RETURNING semesterid`,
         [
             userId,
@@ -41,52 +74,37 @@ export async function createSemester(
     return result.rows[0].semesterid;
 }
 
-export async function getSemesters(
-    userId: number, // eslint-disable-line
-    institutionId: number, // eslint-disable-line
-    programName: string // eslint-disable-line
-): Promise<SemesterInformation[]> {
-    // get user, program from auth system
-    // return all semesters belonging to the user in this program
-    console.log(`We are reading semester information`);
-    return [
-        {
-            semesterId: 1,
-            semesterName: "1a",
-            semesterYear: new Date(2024, 0, 1),
-            semesterTerm: SemesterTerm.FA,
-        },
-        {
-            semesterId: 2,
-            semesterName: "2a",
-            semesterYear: new Date(2024, 0, 1),
-            semesterTerm: SemesterTerm.WI,
-        },
-    ];
-}
-
 export async function updateSemester(
+    userId: number,
     semesterId: number,
     semesterName: string,
     semesterYear: Date,
     semesterTerm: SemesterTerm
-): Promise<number> {
-    // validate semesterid is integer, semeseter name is string of correct length, semester year is a valid year, and semseter term is in the four codes
-    // get user, program from auth system
-    // ensure that user and program correspond to the current semester provided
-    //      if it doesn't exist return a 400 code
-    // update semester information
-    // (i believe these fields aren't used anywhere else so they don't need to be updated anywhere else)
-    console.log(
-        `Update semester ${semesterId} with ${semesterName}, ${semesterYear}, and ${semesterTerm}`
+): Promise<void> {
+    await sql.query(
+        `
+        UPDATE semester
+        SET semestername = $1, semesteryear = $2, semesterterm = $3
+        WHERE userId = $4 AND semesterid = $5;
+        `,
+        [
+            semesterName,
+            semesterYear.getFullYear(),
+            semesterTerm,
+            userId,
+            semesterId,
+        ]
     );
-    return 1;
 }
 
-export async function deleteSemester(semesterId: number): Promise<void> {
-    // validate semseterid is an integer
-    // get user, program from auth system
-    // ensure that semester exists
-    // delete semester
-    console.log(`Delete semester with ${semesterId}`);
+export async function deleteSemester(
+    userId: number,
+    semesterId: number
+): Promise<void> {
+    await sql.query(
+        `
+        DELETE FROM semester
+        WHERE userId = $1 AND semesterid = $2;`,
+        [userId, semesterId]
+    );
 }
