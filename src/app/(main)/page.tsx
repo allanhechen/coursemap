19aaf138034e4af71c2e316ec05f1a759d937b84
Allan He-Chen <allanhechen@gmail.com>
2025-01-26 10:53:05 -0500

    Make the hero page friendlier for now

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import WideLogo from "@/components/header/WideLogo";
+import { Button, Title, Paper } from "@mantine/core";
 
 export default async function Page() {
     const session = await auth();
@@ -12,9 +14,23 @@ export default async function Page() {
     }
 
     return (
-        <h1 className="text-3xl font-bold">
-            This will be the hero page, head to{" "}
-            <Link href={"/dashboard"}>/dashboard</Link> for the dashboard
-        </h1>
+        <div className="h-screen w-screen flex flex-col items-center justify-center">
+            <Paper
+                h={300}
+                w={525}
+                shadow="sm"
+                radius="lg"
+                className="p-5 flex flex-col items-center justify-center"
+            >
+                <WideLogo />
+                <Title order={2} className="mt-5">
+                    This will be the hero page in the future! For now, head to
+                    the signin page!
+                </Title>
+                <Link href="/signin" className="mt-5">
+                    <Button>Go to /signin</Button>
+                </Link>
+            </Paper>
+        </div>
     );
 }
