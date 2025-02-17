b8e823c4fc4f16637c7af65f166fc6b495f90c5c
Allan He-Chen <allanhechen@gmail.com>
2025-02-17 11:15:43 -0500

    Add an option to hide the "Add Semester" button in the navbar

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -6,11 +6,11 @@ import SemesterForm from "@/components/semester/SemesterForm";
 import ThemeSwitcher from "@/components/header/ThemeSwitcher";
 import UserMenu from "@/components/header/UserMenu";
 
-export default function NavBar() {
+export default function NavBar({ hideForm }: { hideForm?: boolean }) {
     return (
         <Paper
             shadow="sm"
-            className="flex justify-between items-center py-3 px-6 m-5 relative"
+            className="flex justify-between items-center py-3 px-6 m-5 relative flex-initial"
             radius="lg"
         >
             <div className="hidden md:block">
@@ -20,7 +20,7 @@ export default function NavBar() {
                 <CompactLogo />
             </div>
             <Group gap="xs" className="items-center">
-                <SemesterForm />
+                {hideForm ? "" : <SemesterForm />}
                 <ThemeSwitcher />
                 <UserMenu />
             </Group>
