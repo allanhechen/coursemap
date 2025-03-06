d006a6aff5bdf5e5d04f02b1d598924748ced16d
Allan He-Chen <allanhechen@gmail.com>
2025-03-06 00:13:33 -0500

    Update Wide Logo to support no link

--- src/components/header/WideLogo.tsx
+++ src/components/header/WideLogo.tsx
@@ -4,19 +4,38 @@ import Link from "next/link";
 import darkWideLogo from "@/../public/logos/coursemap-banner-dark.svg";
 import lightWideLogo from "@/../public/logos/coursemap-banner-light.svg";
 
-export default function WideLogo() {
-    return (
-        <Link href="/dashboard" prefetch>
-            <Image
-                className="hidden dark:block"
-                src={darkWideLogo}
-                alt="Wide Coursemap Logo Dark"
-            />
-            <Image
-                className="dark:hidden"
-                src={lightWideLogo}
-                alt="Wide Coursemap Logo Light"
-            />
-        </Link>
-    );
+export default function WideLogo({ link = true }: { link?: boolean }) {
+    if (!link) {
+        return (
+            <>
+                <Image
+                    className="hidden dark:block"
+                    src={darkWideLogo}
+                    alt="Wide Coursemap Logo Dark"
+                />
+                <Image
+                    className="dark:hidden"
+                    src={lightWideLogo}
+                    alt="Wide Coursemap Logo Light"
+                />
+
+            </>
+        )
+    }
+    else {
+        return (
+            <Link href="/dashboard" prefetch>
+                <Image
+                    className="hidden dark:block"
+                    src={darkWideLogo}
+                    alt="Wide Coursemap Logo Dark"
+                />
+                <Image
+                    className="dark:hidden"
+                    src={lightWideLogo}
+                    alt="Wide Coursemap Logo Light"
+                />
+            </Link>
+        )
+    }
 }
